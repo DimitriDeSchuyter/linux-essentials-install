db68ed45eb166c554604e4fd84e96e2317aa08c1
Frederic Depuydt <frederic.depuydt@outlook.com>
2020-09-03 20:40:55 +0000

    Changed file names to prevent inclusion in malicious practice

--- bash/append.py
+++ bash/append.py
@@ -9,7 +9,7 @@ from depuydt import echo, file
 
 echo.section("Linux essentials - INSTALL", "Appending bash files contents")
 
-file.append(".bash_aliases","~/.bash_aliases")
-file.append(".bash_env","~/.bash_env")
-file.append(".bash_logout","~/.bash_logout")
-file.append(".bashrc","~/.bashrc")
+file.append("bash_aliases","~/.bash_aliases")
+file.append("bash_env","~/.bash_env")
+file.append("bash_logout","~/.bash_logout")
+file.append("bashrc","~/.bashrc")
